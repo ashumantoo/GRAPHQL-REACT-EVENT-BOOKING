c244066966cafc41a158f36df75f6a23348282ac
ashutosh <ashumantoo@gmail.com>
2020-04-11 13:22:00 +0530

    user model added and relations between user and event implemented.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { buildSchema } = require('graphql');
 
 const Event = require('./model/event');
+const User = require('./model/user');
 
 const app = express();
 
@@ -24,11 +26,22 @@ app.use('/graphql', graphqlHttp({
             date:String!
         }
 
+        type User {
+            _id:ID!
+            email:String!
+            password:String
+        }
+
         input EventInput {
             title:String!
             description:String! 
             price:Float!
             date:String!
+        }
+
+        input UserInput {
+            email:String!
+            password:String!
         }
 
         type RootQuery {
@@ -37,6 +50,7 @@ app.use('/graphql', graphqlHttp({
 
         type RootMutation {
             createEvent(eventInput:EventInput):Event
+            createUser(userInput:UserInput):User
         }
 
         schema {
@@ -52,31 +66,64 @@ app.use('/graphql', graphqlHttp({
             return Event.find().then(events => {
                 return events.map(event => {
                     return { ...event._doc, _id: event.id } //in event.id, this id is created by graphql for query operation,
-                                                            // somewhat it is similar to the _id of the mongodb 
-                })
+                    // somewhat it is similar to the _id of the mongodb 
+                });
             }).catch(error => {
                 console.log(error);
                 throw error;
             });
-            return events;
         },
         createEvent: (args) => {
             const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: "5e915fcb4bdb2d34bb3250d6"
             });
-
+            let createdEvent;
             //this return will ensure that some asynchronous task is running, only return after the finishing
             //of the asynchronous task
             return event.save().then(event => {
                 console.log(event);
                 //mongodb _id is somehow not understandable by graphql,so it need to be converted into String 
-                return { ...event._doc, _id: event._doc._id.toString() }
+                createdEvent = { ...event._doc, _id: event._doc._id.toString() }
+                return User.findById("5e915fcb4bdb2d34bb3250d6");
+            }).then(user => {
+                if (!user) {
+                    throw new Error("User not found");
+                }
+                //below code is similar to user.createEvents.push(createdEvent._id)
+                //mongoose will able to pull out _id from the event object becasue in our user schema we are
+                //only storing the createdEvent Id
+                user.createdEvents.push(event);
+                return user.save();
+            }).then(resutl => {
+                return createdEvent;
             }).catch(error => {
                 console.log(error);
                 throw error;
+            });
+        },
+        createUser: (args) => {
+            return User.findOne({ email: args.userInput.email }).then(user => {
+                if (user) {
+                    throw new Error('Email already exists');
+                }
+                return bcrypt.hash(args.userInput.password, 12);
+            }).then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+                // user.save().then().catch();
+                return user.save();
+            }).then(user => {
+                console.log(user);
+                return { ...user._doc, _id: user.id, password: null };
+            }).catch(err => {
+                console.log(err);
+                throw err;
             });
         }
     },
