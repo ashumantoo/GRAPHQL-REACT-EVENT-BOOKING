995d456c9249c9b0e761f29ecbe6491cf01330c5
ashutosh <ashumantoo@gmail.com>
2020-04-12 16:52:50 +0530

    react fronend and pages are added.

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AuthPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Authentication Page</h2>
+            </div>
+        )
+    }
+}
+
+export default AuthPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Events Page</h2>
+            </div>
+        )
+    }
+}
+
+export default EventPage;
