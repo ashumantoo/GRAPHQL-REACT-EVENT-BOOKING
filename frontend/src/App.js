13ec71780cc6e223ab585d9ea1aa59ff0c05add7
ashutosh <ashumantoo@gmail.com>
2020-04-12 17:32:38 +0530

    react navigation added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import MainNavigation from './components/MainNavigation/MainNavigation';
+
 import Auth from './pages/Auth';
 import Events from './pages/Events';
 import Bookings from './pages/Bookings';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={Auth} />
-          <Route path="/events" component={Events} />
-          <Route path="/bookings" component={Bookings} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={Auth} />
+              <Route path="/events" component={Events} />
+              <Route path="/bookings" component={Bookings} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
